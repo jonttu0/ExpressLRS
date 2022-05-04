012dfbae6f90fd78f7a8f6be7927b86052f9a89e
cruwaller <cruwaller@gmail.com>
2022-05-04 23:28:14 +0300

    esp82xx: some hal tweaking

--- src/src/esp82xx/HwSpi.cpp
+++ src/src/esp82xx/HwSpi.cpp
@@ -3,12 +3,172 @@
 #include <Arduino.h>
 #include <SPI.h>
 
+#define USE_ARDUINO_SPI 0
+
+#if USE_ARDUINO_SPI
 SPIClass SpiBus;
+#else
+#include <esp8266_peri.h>
+
+typedef union {
+        uint32_t regValue;
+        struct {
+                unsigned regL :6;
+                unsigned regH :6;
+                unsigned regN :6;
+                unsigned regPre :13;
+                unsigned regEQU :1;
+        };
+} spiClk_t;
+
+void setClockDivider(uint32_t clockDiv) {
+    if(clockDiv == 0x80000000) {
+        GPMUX |= (1 << 9); // Set bit 9 if sysclock required
+    } else {
+        GPMUX &= ~(1 << 9);
+    }
+    SPI1CLK = clockDiv;
+}
+
+static uint32_t ClkRegToFreq(spiClk_t * reg) {
+    return (ESP8266_CLOCK / ((reg->regPre + 1) * (reg->regN + 1)));
+}
+
+void setFrequency(uint32_t freq) {
+    static uint32_t lastSetFrequency = 0;
+    static uint32_t lastSetRegister = 0;
+
+    if (freq >= ESP8266_CLOCK) {
+        // magic number to set spi sysclock bit (see below.)
+        setClockDivider(0x80000000);
+        return;
+    }
+
+    if (lastSetFrequency == freq && lastSetRegister == SPI1CLK) {
+        // do nothing (speed optimization)
+        return;
+    }
+
+    const spiClk_t minFreqReg = { 0x7FFFF020 };
+    uint32_t minFreq = ClkRegToFreq((spiClk_t*) &minFreqReg);
+    if (freq < minFreq) {
+        // use minimum possible clock regardless
+        setClockDivider(minFreqReg.regValue);
+        lastSetRegister = SPI1CLK;
+        lastSetFrequency = freq;
+        return;
+    }
+
+    uint8_t calN = 1;
+
+    spiClk_t bestReg = { 0 };
+    int32_t bestFreq = 0;
+
+    // aka 0x3F, aka 63, max for regN:6
+    const uint8_t regNMax = (1 << 6) - 1;
+
+    // aka 0x1fff, aka 8191, max for regPre:13
+    const int32_t regPreMax = (1 << 13) - 1;
+
+    // find the best match for the next 63 iterations
+    while(calN <= regNMax) {
+
+        spiClk_t reg = { 0 };
+        int32_t calFreq;
+        int32_t calPre;
+        int8_t calPreVari = -2;
+
+        reg.regN = calN;
+
+        while(calPreVari++ <= 1) { // test different variants for Pre (we calculate in int so we miss the decimals, testing is the easyest and fastest way)
+            calPre = (((ESP8266_CLOCK / (reg.regN + 1)) / freq) - 1) + calPreVari;
+            if(calPre > regPreMax) {
+                reg.regPre = regPreMax;
+            } else if(calPre <= 0) {
+                reg.regPre = 0;
+            } else {
+                reg.regPre = calPre;
+            }
+
+            reg.regL = ((reg.regN + 1) / 2);
+            // reg.regH = (reg.regN - reg.regL);
+
+            // test calculation
+            calFreq = ClkRegToFreq(&reg);
+            //os_printf("-----[0x%08X][%d]\t EQU: %d\t Pre: %d\t N: %d\t H: %d\t L: %d = %d\n", reg.regValue, freq, reg.regEQU, reg.regPre, reg.regN, reg.regH, reg.regL, calFreq);
+
+            if(calFreq == static_cast<int32_t>(freq)) {
+                // accurate match use it!
+                memcpy(&bestReg, &reg, sizeof(bestReg));
+                break;
+            } else if(calFreq < static_cast<int32_t>(freq)) {
+                // never go over the requested frequency
+                auto cal = std::abs(static_cast<int32_t>(freq) - calFreq);
+                auto best = std::abs(static_cast<int32_t>(freq) - bestFreq);
+                if(cal < best) {
+                    bestFreq = calFreq;
+                    memcpy(&bestReg, &reg, sizeof(bestReg));
+                }
+            }
+        }
+        if(calFreq == static_cast<int32_t>(freq)) {
+            // accurate match use it!
+            break;
+        }
+        calN++;
+    }
+
+    // os_printf("[0x%08X][%d]\t EQU: %d\t Pre: %d\t N: %d\t H: %d\t L: %d\t - Real Frequency: %d\n", bestReg.regValue, freq, bestReg.regEQU, bestReg.regPre, bestReg.regN, bestReg.regH, bestReg.regL, ClkRegToFreq(&bestReg));
+
+    setClockDivider(bestReg.regValue);
+    lastSetRegister = SPI1CLK;
+    lastSetFrequency = freq;
+}
+
+void setDataMode(uint8_t dataMode) {
+
+    /**
+     SPI_MODE0 0x00 - CPOL: 0  CPHA: 0
+     SPI_MODE1 0x01 - CPOL: 0  CPHA: 1
+     SPI_MODE2 0x10 - CPOL: 1  CPHA: 0
+     SPI_MODE3 0x11 - CPOL: 1  CPHA: 1
+     */
+
+    bool CPOL = (dataMode & 0x10); ///< CPOL (Clock Polarity)
+    bool CPHA = (dataMode & 0x01); ///< CPHA (Clock Phase)
+
+    // https://github.com/esp8266/Arduino/issues/2416
+    // https://github.com/esp8266/Arduino/pull/2418
+    if (CPOL)         // Ensure same behavior as
+        CPHA ^= 1;    // SAM, AVR and Intel Boards
+
+    if (CPHA) {
+        SPI1U |= (SPIUSME);
+    } else {
+        SPI1U &= ~(SPIUSME);
+    }
+
+    if (CPOL) {
+        SPI1P |= 1<<29;
+    } else {
+        SPI1P &= ~(1<<29);
+        //todo test whether it is correct to set CPOL like this.
+    }
+}
+
+inline void setDataBits(uint16_t bits) {
+    const uint32_t mask = ~((SPIMMOSI << SPILMOSI) | (SPIMMISO << SPILMISO));
+    bits--;
+    SPI1U1 = ((SPI1U1 & mask) | ((bits << SPILMOSI) | (bits << SPILMISO)));
+}
+
+#endif
 
 struct spi_config IRAM_ATTR
 spi_setup(uint32_t speed, int sck, int miso, int mosi, uint8_t mode)
 {
     uint8_t datamode[4] = {SPI_MODE0, SPI_MODE1, SPI_MODE2, SPI_MODE3};
+#if USE_ARDUINO_SPI
     if (SpiBus.pins(sck, miso, mosi, -1)) {
         SpiBus.begin();
         SpiBus.setHwCs(false);
@@ -18,23 +178,61 @@ spi_setup(uint32_t speed, int sck, int miso, int mosi, uint8_t mode)
         return {.spi = &SpiBus};
     }
     return {.spi = NULL};
+#else
+    // pins()
+    pinMode(sck, SPECIAL);  ///< GPIO14
+    pinMode(miso, SPECIAL); ///< GPIO12
+    pinMode(mosi, SPECIAL); ///< GPIO13
+
+    // begin()
+    SPI1C = 0;
+    setFrequency(speed);
+    SPI1U = SPIUMOSI | SPIUDUPLEX | SPIUSSE;
+    SPI1U1 = (7 << SPILMOSI) | (7 << SPILMISO); // 8bits
+    SPI1C1 = 0;
+
+    // disable HW CS
+    SPI1U &= ~(SPIUCSSETUP | SPIUCSHOLD);
+
+    // MSB first = SPI1C &= ~(SPICWBO | SPICRBO);
+    // LSB first = SPI1C |= (SPICWBO | SPICRBO);
+
+    setDataMode(datamode[mode]);
+    //setDataBits(8);
+    return {.spi = (void*)&SPI1CMD};
+#endif
 }
 
 void IRAM_ATTR
 spi_prepare(struct spi_config config)
 {
+#if USE_ARDUINO_SPI
     (void)config;
+#else
+#endif
 }
 
 void IRAM_ATTR
 spi_transfer(struct spi_config config, uint8_t receive_data,
              uint8_t len, uint8_t *data)
 {
-    if (config.spi) {
-        SPIClass * spi = (SPIClass*)config.spi;
+    if (!config.spi) return;
+#if USE_ARDUINO_SPI
+    SPIClass * spi = (SPIClass*)config.spi;
+    if (receive_data)
+        spi->transfer(data, len);
+    else
+        spi->writeBytes(data, len);
+#else
+    uint8_t rdata;
+    while (len--) {
+        SPI1W0 = *data;
+        SPI1CMD |= SPIBUSY;
+        while (SPI1CMD & SPIBUSY) {}
+        rdata = (uint8_t) (SPI1W0 & 0xff);
         if (receive_data)
-            spi->transfer(data, len);
-        else
-            spi->writeBytes(data, len);
+            *data = rdata;
+        data++;
     }
+#endif
 }
--- src/src/esp82xx/gpio.cpp
+++ src/src/esp82xx/gpio.cpp
@@ -1,83 +1,102 @@
 #include "gpio.h"
 #include <Arduino.h>
+#include <user_interface.h>
 
-struct gpio_out IRAM_ATTR gpio_out_setup(uint32_t pin, uint32_t val)
+static inline uint8_t IRAM_ATTR _gpio_read(uint8_t const pin)
 {
-    if (160 <= pin)
+    if (pin < 16) {
+        return !!(GPI & (1 << pin));
+    }
+    return GP16I & 0x1;
+}
+
+
+struct gpio_out IRAM_ATTR gpio_out_setup(uint32_t const pin, uint32_t const val)
+{
+    if (16 < pin)
         return {.pin = GPIO_PIN_IVALID};
+
     struct gpio_out g = {.pin = pin};
     gpio_out_reset(g, val);
     return g;
 }
 
-void IRAM_ATTR gpio_out_reset(struct gpio_out g, uint32_t val)
+void IRAM_ATTR gpio_out_reset(struct gpio_out const g, uint32_t const val)
 {
     pinMode(g.pin, OUTPUT);
-    digitalWrite(g.pin, val);
+    gpio_out_write(g, val);
 }
 
-void IRAM_ATTR gpio_out_toggle_noirq(struct gpio_out g)
+void IRAM_ATTR gpio_out_toggle_noirq(struct gpio_out const g)
 {
-    digitalWrite(g.pin, !digitalRead(g.pin));
+    gpio_out_write(g, !_gpio_read(g.pin));
 }
 
-void IRAM_ATTR gpio_out_toggle(struct gpio_out g)
+void IRAM_ATTR gpio_out_toggle(struct gpio_out const g)
 {
-    digitalWrite(g.pin, !digitalRead(g.pin));
+    gpio_out_toggle_noirq(g);
 }
 
-void IRAM_ATTR gpio_out_write(struct gpio_out g, uint32_t val)
+void IRAM_ATTR gpio_out_write(struct gpio_out const g, uint32_t const val)
 {
-    digitalWrite(g.pin, val);
+    if (g.pin < 16) {
+        if (val) GPOS = (1 << g.pin);
+        else     GPOC = (1 << g.pin);
+    } else {
+        if (val) GP16O |= 1;
+        else     GP16O &= ~1;
+    }
 }
 
 
-struct gpio_in IRAM_ATTR gpio_in_setup(uint32_t pin, int32_t pull_up)
+struct gpio_in IRAM_ATTR gpio_in_setup(uint32_t const pin, int32_t const pull_up)
 {
-    if (160 <= pin)
+    if (16 < pin)
         return {.pin = GPIO_PIN_IVALID};
     struct gpio_in g = {.pin = pin};
     gpio_in_reset(g, pull_up);
     return g;
 }
 
-void IRAM_ATTR gpio_in_reset(struct gpio_in g, int32_t pull_up)
+void IRAM_ATTR gpio_in_reset(struct gpio_in const g, int32_t const pull_up)
 {
     pinMode(g.pin, (pull_up) ? INPUT_PULLUP : INPUT);
 }
 
-uint8_t IRAM_ATTR gpio_in_read(struct gpio_in g)
+uint8_t IRAM_ATTR gpio_in_read(struct gpio_in const g)
 {
-    return digitalRead(g.pin);
+    return _gpio_read(g.pin);
 }
 
-void IRAM_ATTR gpio_in_isr(struct gpio_in g, isr_cb_t func, uint8_t type)
+void IRAM_ATTR gpio_in_isr(struct gpio_in const g, isr_cb_t func, uint8_t const type)
 {
     attachInterrupt(digitalPinToInterrupt(g.pin), func, type);
 }
 
-void IRAM_ATTR gpio_in_isr_remove(struct gpio_in g)
+void IRAM_ATTR gpio_in_isr_remove(struct gpio_in const g)
 {
     detachInterrupt(digitalPinToInterrupt(g.pin));
 }
 
-void IRAM_ATTR gpio_in_isr_clear_pending(struct gpio_in g)
+void IRAM_ATTR gpio_in_isr_clear_pending(struct gpio_in const g)
 {
     if (gpio_in_valid(g)) {
-        int pin = digitalPinToInterrupt(g.pin);
-        if (pin < 0)
-            return;
-        GPIEC = (1 << pin); //Clear Interrupt
+        int const pin = digitalPinToInterrupt(g.pin);
+        if (0 <= pin)
+            GPIEC = (1 << pin); //Clear Interrupt
     }
 }
 
-struct gpio_adc gpio_adc_setup(uint32_t pin)
+struct gpio_adc gpio_adc_setup(uint32_t const pin)
 {
-    struct gpio_adc g = {.pin = pin};
-    return g;
+    if (pin == 17 || pin == 0)
+        return {.pin = pin};
+    return {.pin = GPIO_PIN_IVALID};
 }
 
-uint32_t gpio_adc_read(struct gpio_adc g)
+uint32_t gpio_adc_read(struct gpio_adc const g)
 {
-    return analogRead(g.pin);
+    if (g.pin != GPIO_PIN_IVALID)
+        return system_adc_read();
+    return 0;
 }
